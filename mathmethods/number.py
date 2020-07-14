1102d47408ecbc3cb9bd3089b7f4c57c1c8c3ffb
Mike Verheijden <mikev97@hotmail.com>
2020-07-14 11:29:02 +0200

    refined

--- mathmethods/number.py
+++ mathmethods/number.py
@@ -4,7 +4,7 @@ logging.basicConfig(level=logging.INFO)
 class Number:
     def __init__(self, num):
         self.num = num
-        logging.info("Student initiated.")
+        logging.info("Number initiated.")
         
     def get_number(self):
         print(f'Number is "{self.num}"')
@@ -12,16 +12,24 @@ class Number:
     def squared(self):
         logging.info(f'Multiplying the squared variables of {self.num} is {self.num**2}')
         logging.debug(f'Square variable of {self.num} = {self.num**2}')
-        return self**2
+        return self.num**2
     
-    def addition(self, addition):
-        logging.debug(f'Number {self.num} + {addition}')
-        return self+addition
+    def addition(self, additor):
+        logging.debug(f'Number {self.num} + {additor}')
+        return self.num+additor
     
-    def substraction(self, substraction):
-        logging.debug(f'Number {self.num} - {substraction}')
-        return self.num-substraction
+    def substraction(self, substractor):
+        logging.debug(f'Number {self.num} - {substractor}')
+        return self.num-substractor
     
     def division(self, divider):
         return self.num/divider
+    
+    def is_even(self):
+        if self.num % 2 == 0:
+            logging.info(f"{self.num} is an even number")
+            return True
+        else:
+            logging.info(f"{self.num} is an odd number")
+            return False
 
